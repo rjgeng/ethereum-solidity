34e9c08648df76ce56c1400cede20c3235efbca0
RFNoC <rj.geng@gmail.com>
2022-08-21 08:00:51 -0700

    lottery-react - Instance Properties

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -4,11 +4,10 @@ import web3 from './web3';
 import lottery from "./lottery";
  
 class App extends React.Component {
-  constructor (props) {
-    super(props);
-
-    this.state = { manager: ''};
-  }
+  state = {
+    manager: '',
+    
+  };
 
   async componentDidMount(){
     const manager = await lottery.methods.manager().call();
