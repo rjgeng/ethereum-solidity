4cb1e7e3c1158e787aba5d72ffa1cd683a0b6b51
RFNoC <rj.geng@gmail.com>
2022-08-21 11:15:01 -0700

    lottery-react - Form Setup

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -19,6 +19,17 @@ class App extends React.Component {
     this.setState({ manager, players, balance});
   }
 
+  onSubmit = async (event) =>{
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.event().send ({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    })
+  }
+
   render() {    
     return (
       <div>
@@ -31,7 +42,7 @@ class App extends React.Component {
 
           <hr />
 
-          <form>
+          <form onSubmit={this.onSubmit}>
             <h4>Want to try your luck?</h4>
             <div>
               <label>Amount of ether to enter</label>
