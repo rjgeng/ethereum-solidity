2da74ad4d7ee486d5d08a807f4055e799756eade
RFNoC <rj.geng@gmail.com>
2022-08-24 15:09:40 -0700

    kickstart - Accessing Mappings

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -35,5 +35,19 @@ describe('[Campaigns', () => {
     it('deploys a factory and a campain', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
-    });    
-  })
+    });  
+    
+    it ('marks caller as teh campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it ('allows people to contribute monfey and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });  
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();  
+        assert(isContributor);
+    })
+})
