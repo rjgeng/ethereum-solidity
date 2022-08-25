86eaa76dda4db6fc557403a34cbf93e3eca98bea
RFNoC <rj.geng@gmail.com>
2022-08-25 08:23:12 -0700

    kickstart - metamaskSRP and endpoints in secret

---
+++ kickstart/ethereum/secret/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config() 
+
+module.exports = {
+  metamaskSRP: process.env.metamaskSRP,
+  infuraRinkebyEndpoint: process.env.infuraRinkebyEndpoint,
+  infuraGorliEndpoint: process.env.infuraGorliEndpoint,
+}
