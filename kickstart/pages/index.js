61c56803c444cb4abded80404bbe725e9338934a
RFNoC <rj.geng@gmail.com>
2022-08-27 14:22:12 -0700

    kickstart - The Layout Component

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -8,7 +9,6 @@ class CampaignIndex extends Component {
 
     return { campaigns };
   }
-
   renderCampaigns() {
     const items = this.props.campaigns.map((address) => {
       return {
@@ -19,21 +19,20 @@ class CampaignIndex extends Component {
     });
     return <Card.Group items={items} />;
   }
-
   render() {
-    return <div>
-      <link
-        rel="stylesheet"
-        href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
-      />    
-      <h3>Open Campaigns</h3>  
-      {this.renderCampaigns()}
-      <Button 
-        content="Create Campain"
-        icon = "add circle"
-        primary
-      />
-      </div>;
+    return (
+      <Layout>
+        <div>
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+          ></link>
+          <h3>Open Campaigns</h3>
+          {this.renderCampaigns()}
+          <Button content="Create Campaign" icon="add circle" primary />
+        </div>
+      </Layout>
+    );
   }
 }
 
