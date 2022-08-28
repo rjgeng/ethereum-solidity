67dfa78e7ea45d1b9fdd7da78621be01d8350a86
RFNoC <rj.geng@gmail.com>
2022-08-28 16:37:21 -0700

    kickstart - Input Change Handlers: 186

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
-import { Form, Button } from "semantic-ui-react";
-import Layout from '../../components/Layout';
+import { Form, Button, Input } from "semantic-ui-react";
+import Layout from "../../components/Layout";
 
 class CampaignNew extends Component {
-    render(){
-        return (
-            <Layout>
-                <h3>Create a Campaign</h3>
+  state = {
+    minimumContribution: "",
+  };
 
-                <Form>
-                    <Form.Field>
-                        <label>Minimum Contribution</label>
-                        <input />
-                    </Form.Field>
-
-                    <Button primary>Create!</Button>
-                </Form>
-            </Layout>        
-        )
-    }
+  render() {
+    return (
+      <Layout>
+        <h3>Create Campaign</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={(event) =>
+                this.setState({ minimumContribution: event.target.value })
+              }
+            />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
+      </Layout>
+    );
+  }
 }
 
 export default CampaignNew;
