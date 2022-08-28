1cefbafd02d4e542a5074279e7e47a475969bd42
RFNoC <rj.geng@gmail.com>
2022-08-28 15:11:33 -0700

    kickstart - Final CSS Fix: 184

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -22,11 +22,7 @@ class CampaignIndex extends Component {
   render() {
     return (
       <Layout>
-        <div>
-          <link
-            rel="stylesheet"
-            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
-          ></link>
+        <div>          
           <h3>Open Campaigns</h3>          
           <Button 
           floated = "right" 
