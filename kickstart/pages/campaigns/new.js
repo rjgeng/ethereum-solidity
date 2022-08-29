d580f8a0d50173d93906dbfb9a8ad30ed9e6397d
RFNoC <rj.geng@gmail.com>
2022-08-29 08:16:07 -0700

    kickstart - Button Spinners: 190

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -8,10 +8,14 @@ class CampaignNew extends Component {
   state = {
     minimumContribution: "",
     errorMessage: "",
+    loading: false
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
+
+    this.setState({ loading: true });
+
     try {
       const accounts = await web3.eth.getAccounts();
       await factory.methods
@@ -22,6 +26,8 @@ class CampaignNew extends Component {
     } catch (err) {
       this.setState({ errorMessage: err.message });
     }
+
+    this.setState({ loading: false });
   };
 
   render() {
@@ -41,7 +47,7 @@ class CampaignNew extends Component {
             />
           </Form.Field>
           <Message error header="Oops!" content={this.state.errorMessage} />
-          <Button primary>Create!</Button>
+          <Button loading = {this.state.loading} primary>Create!</Button>
         </Form>
       </Layout>
     );
