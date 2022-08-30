27a1a6ae48fca46d9b7802829798e2c7d3b6a98b
RFNoC <rj.geng@gmail.com>
2022-08-30 11:23:22 -0700

    kickstart - Custom Card Groups: 204

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
 
@@ -8,14 +9,42 @@ class CampaignShow extends Component {
 
     const summary = await campaign.methods.getSummary().call();
 
-    console.log(summary);
-
-    return {};
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
   }
+
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount,
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: "Address of Manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: { overflowWrap: "break-word" },
+      },
+    ];
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
